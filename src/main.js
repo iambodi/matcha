6e2dff95376c304c4b0ae472043b24802da0ae12
Iscia <iscia.bodi@gmail.com>
2019-11-01 14:08:10 +0100

    Just commented

--- src/main.js
+++ src/main.js
@@ -3,14 +3,14 @@ import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify';
 import axios from 'axios'
-import socketio from 'socket.io-client';
-import VueSocketIO from 'vue-socket.io';
+// import socketio from 'socket.io-client';
+// import VueSocketIO from 'vue-socket.io';
 
-const SocketInstance = socketio.connect('http://localhost:5000');
-Vue.use(new VueSocketIO({
-  debug: true,
-  connection: SocketInstance
-}));
+// const SocketInstance = socketio.connect('http://localhost:5000');
+// Vue.use(new VueSocketIO({
+//   debug: true,
+//   connection: SocketInstance
+// }));
 // Vue.use(VueSocketIO, SocketInstance)
 
 window.axios = require('axios');
