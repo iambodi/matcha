91e16e22d60898ff695cfba179b42c648c511264
Thomas Oliver <blueskin@blueskins-macbook.local>
2019-11-01 14:16:13 +0100

    save

--- src/main.js
+++ src/main.js
@@ -7,12 +7,7 @@ import socketio from 'socket.io-client';
 // import VueSocketIO from 'vue-socket.io';
 
 const SocketInstance = socketio.connect('http://localhost:5000');
->>>>>>> f4cd2a4acfae0eef7ee8816cebb4e02aab214393
-// Vue.use(new VueSocketIO({
-//   debug: true,
-//   connection: SocketInstance
-// }));
-// Vue.use(VueSocketIO, SocketInstance)
+
 
 window.axios = require('axios');
 Vue.config.productionTip = false
